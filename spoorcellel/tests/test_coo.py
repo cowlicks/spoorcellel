de2bbd4b6561ab9cde9456d48bfaa12acc37a020
Blake Griffith <blake.a.griffith@gmail.com>
2016-01-05 11:54:51 +0100

    test COO toarray and fromarray

--- spoorcellel/tests/test_coo.py
+++ spoorcellel/tests/test_coo.py
@@ -5,3 +5,13 @@ def test_coo_init():
     b = np.ones((10,))
     c = np.ones((10,))
     coo.COO(a, b, c, (10, 10))
+
+def test_coo_fromarray():
+    a = np.arange(100).reshape(10, 10)
+    res = coo.fromarray(a)
+    assert res.shape == (10, 10)
+
+def test_coo_toarray():
+    a = np.arange(100).reshape(10, 10)
+    b = coo.fromarray(a)
+    np.testing.assert_array_equal(b.toarray(), a)
