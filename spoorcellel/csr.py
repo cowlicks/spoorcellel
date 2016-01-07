e3bae81e5dbd9445a6415a5ac2e22fe219c54b60
Blake Griffith <blake.a.griffith@gmail.com>
2016-01-07 19:16:06 +0100

    Add csr.tocoo and toarray

--- spoorcellel/csr.py
+++ spoorcellel/csr.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+from .coo import COO
+
 class CSR:
     def __init__(self, nzs, nzi, ci, shape):
         """
@@ -6,10 +10,30 @@ class CSR:
         ci: column indices
         """
         self.nonzeros = nzs
-        self.nonzero_indices = nzs
+        self.nonzero_indices = nzi
         self.column_indices = ci
         self.shape = shape
 
-    def scalar_elemwise(self, op, args):
-        self.nonzeros = op(self.nonzeros, args)
-        return self
+        self.nnz = len(self.nonzeros)
+
+    def tocoo(self):
+        """
+        This must compute:
+        self.nonzero_indicies
+
+        Suggestions on avoiding computing this?
+        """
+
+        nzi = self.nonzero_indices.compute() # bad
+        row_indices= np.empty(self.nnz) # also bad
+        for i in range(self.nnz):
+            for j in range(nzi[i], nzi[i + 1]):
+                row_indices[j] = i
+
+        return COO(self.nonzeros,
+                   row_indices,
+                   self.column_indices,
+                   self.shape)
+
+    def toarray(self):
+        return self.tocoo().toarray()
