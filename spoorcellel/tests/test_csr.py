610b93a2ee04d603910bbeb7f3eba74a3633dbdf
Blake Griffith <blake.a.griffith@gmail.com>
2016-01-07 19:19:09 +0100

    Test toarray

--- spoorcellel/tests/test_csr.py
+++ spoorcellel/tests/test_csr.py
@@ -1,4 +1,16 @@
+import numpy as np
+import dask.array as da
+
 from spoorcellel import csr
 
-def test_toarray():
+# create 5x5 csr identity matrix
+nzs = da.from_array(np.ones(5), chunks=(5,))
+nzi = da.from_array(np.arange(6), chunks=(6,))
+ci = da.from_array(np.arange(5), chunks=(5,))
+shape = (5, 5)
 
+arr = csr.CSR(nzs, nzi, ci, shape)
+
+def test_toarray():
+    expected = np.identity(5)
+    np.testing.assert_array_equal(arr.toarray(), expected)
