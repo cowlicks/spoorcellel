bf8a081419ed269732a9d1db5ed2fa380cc053af
Blake Griffith <blake.a.griffith@gmail.com>
2016-01-11 17:23:56 +0100

    Test csr scalar multiplication.
    Refactor tests a bit.

--- spoorcellel/tests/test_csr.py
+++ spoorcellel/tests/test_csr.py
@@ -3,20 +3,24 @@ import dask.array as da
 
 from spoorcellel import csr
 
-# create 5x5 csr identity matrix
-nzs = da.from_array(np.ones(5), chunks=(5,))
-nzi = da.from_array(np.arange(6), chunks=(6,))
-ci = da.from_array(np.arange(5), chunks=(5,))
-shape = (5, 5)
-
-arr = csr.CSR(nzs, nzi, ci, shape)
-
 def test_toarray():
+    # create 5x5 csr identity matrix
+    nzs = da.from_array(np.ones(5), chunks=(5,))
+    nzi = da.from_array(np.arange(6), chunks=(6,))
+    ci = da.from_array(np.arange(5), chunks=(5,))
+    shape = (5, 5)
+    arr = csr.CSR(nzs, nzi, ci, shape)
+
     expected = np.identity(5)
     np.testing.assert_array_equal(arr.toarray(), expected)
 
 
 def test_identity():
     expected = np.identity(10)
-    a = csr.identity(10)
-    np.testing.assert_array_equal(a.toarray(), expected)
+    result = csr.identity(10)
+    np.testing.assert_array_equal(result.toarray(), expected)
+
+def test_mul():
+    expected = np.identity(10) * 5
+    result = csr.identity(10) * 5
+    np.testing.assert_array_equal(result.toarray(), expected)
